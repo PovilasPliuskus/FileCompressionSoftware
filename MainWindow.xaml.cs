6a458c6c159df30d6d5a860781754faf869cc921
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-02-19 13:29:52 +0200

    Implemented the functionality to drag and drop files to the drag and drop box

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Win32;
+using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace FileCompressionSoftware
 {
@@ -28,16 +30,70 @@ namespace FileCompressionSoftware
 
         private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            // Changing implementation of openning explorer that it allows to choose a file
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.FilterIndex = 1;
 
             if (openFileDialog.ShowDialog() == true)
             {
+                // selectedFile - the absolute address of the file
                 string selectedFile = openFileDialog.FileName;
-                MessageBox.Show("Selected file: " + selectedFile);
-
+                string fileName = System.IO.Path.GetFileName(selectedFile);
+                DADTextBlock.Text = "Selected file: " + fileName;
+                DADTextBlock.Foreground = Brushes.Black;
+                DADTextBlock.VerticalAlignment = VerticalAlignment.Top;
+                DADTextBlock.HorizontalAlignment= HorizontalAlignment.Left;
+                CompressButton.IsEnabled = true;
+                EncryptButton.IsEnabled = true;
             }
+        }
+
+        // This attribute specifies the method that will be called when a file is dropped onto the container
+        private void Border_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // For simplicity, we only handle the first file
+                string selectedFile = files[0];
+                string fileName = System.IO.Path.GetFileName(selectedFile);
+                DADTextBlock.Text = "Selected file: " + fileName;
+                DADTextBlock.Foreground = Brushes.Black;
+                DADTextBlock.VerticalAlignment = VerticalAlignment.Top;
+                DADTextBlock.HorizontalAlignment = HorizontalAlignment.Left;
+                CompressButton.IsEnabled = true;
+                EncryptButton.IsEnabled = true;
+            }
+        }
+
+        // This attribute specifies the method that will be called when a file is dragged over the container
+        private void Border_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+
+            e.Handled = true;
+        }
+
+        // This attribute specifies the method that will be called repeatedly while a file is being dragged over the container
+        private void Border_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+
+            e.Handled = true;
         }
     }
 
