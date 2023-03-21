35ea100e5e826f6283f21b17beeb679ac79c09d7
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-03-21 22:59:15 +0200

    test

--- LZAlgorithm/main.cpp
+++ LZAlgorithm/main.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    compress(argv[1], argv[2]);
-    //decompress(argv[1], argv[2]);
+    std::cout << argv[0] << std::endl;
+    std::cout << argv[1] << std::endl;
+    std::cout << argv[2] << std::endl;
+    //compress(argv[1], argv[2]);
+    decompress(argv[1], argv[2]);
 
     return 0;
 }
