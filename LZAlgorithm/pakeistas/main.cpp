65722689cc1376d1ab4352ece38e825a16c52b2e
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-03-21 22:20:56 +0200

    test

---
+++ LZAlgorithm/pakeistas/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"LZAlgorithm.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    compress(argv[1], argv[2]);
+    //decompress(argv[1], argv[2]);
+
+    return 0;
+}
