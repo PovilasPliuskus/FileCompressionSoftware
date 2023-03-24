be8e7c35fe52f958fcc3722e6b4628414bd60535
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-03-24 21:45:55 +0200

    executionables

--- LZAlgorithm/main.cpp
+++ LZAlgorithm/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    std::cout << argv[0] << std::endl;
-    std::cout << argv[1] << std::endl;
-    std::cout << argv[2] << std::endl;
+    //std::cout << argv[0] << std::endl;
+    //std::cout << argv[1] << std::endl;
+    //std::cout << argv[2] << std::endl;
     //compress(argv[1], argv[2]);
     decompress(argv[1], argv[2]);
 
