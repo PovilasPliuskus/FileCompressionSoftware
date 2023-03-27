41d8eb537bcae90413bbac3e4e98f8c9b5a69f02
Povilas Pliuskus <PovilasPliuskus@gmail.com>
2023-03-27 17:20:59 +0300

    Redone the Lempel Ziv algorithms to add an extension to decompressed file

---
+++ Decompress.xaml.cs
@@ -0,0 +1,201 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace FileCompressionSoftware
+{
+    /// <summary>
+    /// Interaction logic for Decompress.xaml
+    /// </summary>
+    public partial class Decompress : Window
+    {
+        public Decompress(string selectedFileURL)
+        {
+            InitializeComponent();
+            SelectedDocumentTextBox.Text = selectedFileURL;
+        }
+
+        private void SelectedDocumentButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.FilterIndex = 1;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SelectedDocumentTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void DecompressedDocumentButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            DecompressedDocumentTextBox.Text = saveFileDialog.FileName;
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                DecompressedDocumentTextBox.Text = saveFileDialog.FileName;
+            }
+        }
+        public bool CheckIfValidInput()
+        {
+            if (SelectedDocumentTextBox.Text == "")
+            {
+                MessageBox.Show("No document selected for decompression");
+                return false;
+            }
+            if (DecompressedDocumentTextBox.Text == "")
+            {
+                MessageBox.Show("Please select decompressed document location");
+                return false;
+            }
+            if (!File.Exists(SelectedDocumentTextBox.Text))
+            {
+                MessageBox.Show("Could not locate the selected document for compression");
+                return false;
+            }
+            return true;
+        }
+
+
+        private async void DecompressionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckIfValidInput() == false)
+            {
+                return;
+            }
+
+            if (SelectedDocumentTextBox.Text != null)
+            {
+                string filePath = SelectedDocumentTextBox.Text;
+                string extension = System.IO.Path.GetExtension(filePath);
+
+                if (extension == ".hfm")
+                {
+                    StatusTextBlock.Text = "Compressing";
+                    StatusTextBlock.Background = Brushes.Yellow;
+
+                    string HuffmanAlgorithm = @"..\\..\\..\\HuffmanAlgorithm\\hfmDecompress.exe";
+                    string inputFile = SelectedDocumentTextBox.Text;
+                    string outputFile = DecompressedDocumentTextBox.Text;
+
+                    int estimatedTimeInSeconds = 30;
+
+                    await Task.Run(() =>
+                    {
+                        ProcessStartInfo startInfo = new ProcessStartInfo(HuffmanAlgorithm, $"{inputFile} {outputFile}")
+                        {
+                            CreateNoWindow = true
+                        };
+                        Process process = Process.Start(startInfo);
+
+                        while (!process.WaitForExit(100))
+                        {
+
+                            Dispatcher.Invoke(() =>
+                            {
+                                double progress = 0;
+                                if (estimatedTimeInSeconds > 0)
+                                {
+                                    progress = ((double)process.TotalProcessorTime.Ticks / (double)TimeSpan.TicksPerSecond) / (double)estimatedTimeInSeconds;
+                                }
+                                CompressionProgressBar.Value = progress * 100;
+                            });
+                        }
+
+                        if (process.ExitCode == 0)
+                        {
+                            Dispatcher.Invoke(() =>
+                            {
+                                StatusTextBlock.Text = "Successfully Compressed";
+                                StatusTextBlock.Background = Brushes.Green;
+                                CompressionProgressBar.Value = 100;
+                            });
+                        }
+                        else
+                        {
+                            Dispatcher.Invoke(() =>
+                            {
+                                StatusTextBlock.Text = "Compression Failed";
+                                StatusTextBlock.Background = Brushes.Red;
+                                CompressionProgressBar.Value = 0;
+                            });
+                        }
+                    });
+                }
+                else if (extension == ".lz")
+                {
+                    StatusTextBlock.Text = "Compressing";
+                    StatusTextBlock.Background = Brushes.Yellow;
+
+                    string LZAlgorithm = @"..\\..\\..\\LZAlgorithm\\LZDecompress.exe";
+                    string inputFile = SelectedDocumentTextBox.Text;
+                    string outputFile = DecompressedDocumentTextBox.Text;
+
+                    int estimatedTimeInSeconds = 30;
+
+                    await Task.Run(() =>
+                    {
+                        ProcessStartInfo startInfo = new ProcessStartInfo(LZAlgorithm, $"{inputFile} {outputFile}")
+                        {
+                            CreateNoWindow = true
+                        };
+                        Process process = Process.Start(startInfo);
+
+                        while (!process.WaitForExit(100))
+                        {
+
+                            Dispatcher.Invoke(() =>
+                            {
+                                double progress = 0;
+                                if (estimatedTimeInSeconds > 0)
+                                {
+                                    progress = ((double)process.TotalProcessorTime.Ticks / (double)TimeSpan.TicksPerSecond) / (double)estimatedTimeInSeconds;
+                                }
+                                CompressionProgressBar.Value = progress * 100;
+                            });
+                        }
+
+                        if (process.ExitCode == 0)
+                        {
+                            Dispatcher.Invoke(() =>
+                            {
+                                StatusTextBlock.Text = "Successfully Compressed";
+                                StatusTextBlock.Background = Brushes.Green;
+                                CompressionProgressBar.Value = 100;
+                            });
+                        }
+                        else
+                        {
+                            Dispatcher.Invoke(() =>
+                            {
+                                StatusTextBlock.Text = "Compression Failed";
+                                StatusTextBlock.Background = Brushes.Red;
+                                CompressionProgressBar.Value = 0;
+                            });
+                        }
+                    });
+                }
+                else
+                {
+                    MessageBox.Show("Please enter a file with extensions .hfm or .lz");
+                }
+            }
+        }
+    }
+}
